344640d38daf8fce87d7652c50b86d8b022e8e95
luis guerra <49825068+luisguerra44@users.noreply.github.com>
2020-03-10 13:26:01 -0500

    Create webpack.dev.js

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/`index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.join(__dirname, "src/client/views/index.html"),
+            filename: "./index.html"
+        })
+    ]
+}
