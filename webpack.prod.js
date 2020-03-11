febb7a18710d8daae46b92b24800119b6e4a2ac3
luisguerra44 <luis@Tests-MacBook-Air.local>
2020-03-11 17:11:42 -0500

    running npm run build-dev still putting out error

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,49 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    entry: 'src/client/index.js',
+    entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+      },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template:  "src/client/views/index.html",
+            template:  "./src/client/views/index.html",
             filename: "./index.html"
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: false,
+            protectWebpackAssets: false
+          }),
+          new MiniCssExtractPlugin({
+            filename: '[name].css',
+          }),
     ]
 }
