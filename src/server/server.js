6785b4cb958d18b023b72f9c4687a3af5b9d4f1a
luisguerra44 <leg_44@hotmail.com>
2020-03-20 18:34:14 -0500

    got the final touches going

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,10 @@
-var path = require('path');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 // Endpoint for all routes
-let projectData = [];
+let projectData = {};
 
 // BodyParser config
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.static('dist'));
 
 //Get route
-app.get('/', function (req, res) {
-  res.sendFile('dist/index.html')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 
 // Post Route
 app.post('/add', addInfo);
@@ -36,7 +36,7 @@ function addInfo(req, res) {
 // Setup Server
 
 const port = 8800;
-const server = app.listen(port,listening);
+const server = app.listen(port, listening);
 function listening() {
   console.log(`Server running on localhost: ${port}`);
 }
